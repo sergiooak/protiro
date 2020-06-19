6d0e2710176e0111c3b0a8fb2ccb3a955b163182
Sergio Carvalho <sergiopcn@gmail.com>
2020-06-19 17:42:46 -0300

    novo teste

--- functions/teste/teste.js
+++ functions/teste/teste.js
@@ -1,10 +1,13 @@
+const axios = require('axios');
+
+async function getPage(){
+  return axios.post('https://api.netlify.com/build_hooks/5eed171277c5fc019b2b52d3');
+}
+
 // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
 exports.handler = async (event, context) => {
+  await getPage();
   try {
-    fetch("https://api.netlify.com/build_hooks/5eed171277c5fc019b2b52d3", {
-      method: "POST",
-      body: form
-    });
     return {
       statusCode: 200,
       body: JSON.stringify({ message: `Build Iniciada` })
