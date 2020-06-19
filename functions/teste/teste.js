41e0a306838dd2ca3fafabbb1e50a8437ae13d74
Sergio Carvalho <sergiopcn@gmail.com>
2020-06-19 17:36:43 -0300

    teste

--- functions/teste/teste.js
+++ functions/teste/teste.js
@@ -1,9 +1,13 @@
 // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
 exports.handler = async (event, context) => {
   try {
+    fetch("https://api.netlify.com/build_hooks/5eed171277c5fc019b2b52d3", {
+      method: "POST",
+      body: form
+    });
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: `Teste` })
+      body: JSON.stringify({ message: `Build Iniciada` })
       // // more keys you can return:
       // headers: { "headerName": "headerValue", ... },
       // isBase64Encoded: true,
