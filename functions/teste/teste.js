bc6c8acd01e84a8680f9e57425fe201041b91a53
Sergio Carvalho <sergiopcn@gmail.com>
2020-06-19 17:28:03 -0300

    teste

---
+++ functions/teste/teste.js
@@ -0,0 +1,14 @@
+// Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
+exports.handler = async (event, context) => {
+  try {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: `Teste` })
+      // // more keys you can return:
+      // headers: { "headerName": "headerValue", ... },
+      // isBase64Encoded: true,
+    }
+  } catch (err) {
+    return { statusCode: 500, body: err.toString() }
+  }
+}
